1b643c6e9c36d3a902052821fe3c725055ea4c3f
Hassan Nadeem <58742571+HN-Diablo@users.noreply.github.com>
2025-11-06 14:57:22 +0500

    update

---
+++ lib/typescript/commonjs/src/index.d.ts
@@ -0,0 +1,23 @@
+import { COMMANDS } from './utils/printer-commands';
+export declare enum PrinterWidth {
+    '58mm' = 58,
+    '80mm' = 80
+}
+export interface PrinterImageOptions {
+    imageWidth?: number;
+    imageHeight?: number;
+}
+export declare const WinPrinter: {
+    init(): void;
+    getDeviceList(): Promise<Array<string>>;
+    connectPrinter(deviceId: string): Promise<string>;
+    closeConn(): Promise<void>;
+    printText(text: string, _opts?: any): Promise<void>;
+    printBill(text: string, _opts?: any): Promise<void>;
+    printImage(_imgUrl: string, _opts?: PrinterImageOptions): void;
+    printImageBase64(Base64: string, opts?: PrinterImageOptions): Promise<void>;
+    printRaw(_text: string): void;
+    printColumnsText(_texts: string[], _columnWidth: number[], _columnAlignment: any, _columnStyle: string[], _opts?: any): void;
+};
+export { COMMANDS };
+//# sourceMappingURL=index.d.ts.map
---
+++ lib/typescript/module/src/NativeReactNativeWindowsThermalPrinter.d.ts
@@ -0,0 +1,18 @@
+import type { TurboModule } from 'react-native';
+export interface PrinterOptions {
+    beep?: boolean;
+    cut?: boolean;
+    tailingLine?: boolean;
+    encoding?: string;
+}
+export interface Spec extends TurboModule {
+    getDeviceList(): Promise<Array<string>>;
+    connectPrinter(deviceId: string): Promise<string>;
+    closeConn(): Promise<void>;
+    printText(text: string): Promise<void>;
+    printBill(text: string): Promise<void>;
+    printImageBase64(Base64: string, width: number | undefined, height: number | undefined): Promise<void>;
+}
+declare const _default: Spec;
+export default _default;
+//# sourceMappingURL=NativeReactNativeWindowsThermalPrinter.d.ts.map
